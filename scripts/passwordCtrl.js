74a54c82f8267f71442b1257b2e577c3f48c006e
joshsauder <joshsauder@gmail.com>
2017-10-25 21:34:35 -0400

    Completed password check

---
+++ scripts/passwordCtrl.js
@@ -0,0 +1,19 @@
+var app = angular.module("myApp");
+app.controller('passCtrl', function($scope){
+    $scope.testPass = function(password){
+        $scope.strength = 0;
+        var tests = [/[0-9]/, /[a-z]/, /[A-Z]/, /[~`!#$%\^&*+=\-\[\]\\';,/{}|\\":<>\?]/];
+        if(password.length > 8){
+            $scope.strength++;
+        }
+        for(var i in tests){
+            var testExp = new RegExp(i);
+            if(testExp.test(password)){
+                $scope.strength++;
+            }
+        }
+    }
+    $scope.submit = function(password){
+        password = "";
+    }
+});
