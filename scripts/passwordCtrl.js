2f102f4f376581de00b517a18fe0d1c6f8e2d10c
joshsauder <joshsauder@gmail.com>
2017-11-11 23:25:46 -0500

    Password enumeration complete

--- scripts/passwordCtrl.js
+++ scripts/passwordCtrl.js
@@ -1,5 +1,7 @@
 var app = angular.module("myApp");
 app.controller('passCtrl', function($scope){
+    $scope.passlimit = 0;
+    var passStore = ["A1B2@", "GoBucks", "SaveTheCrew"]
     $scope.testPass = function(password){
         $scope.strength = 0;
         var tests = [/[0-9]/, /[a-z]/, /[A-Z]/, /[~`!#$%\^&*+=\-\[\]\\';,/{}|\\":<>\?]/];
@@ -15,5 +17,18 @@ app.controller('passCtrl', function($scope){
     }
     $scope.submit = function(password){
         $scope.password = null;
+        $scope.passlimit=0;
+    }
+    $scope.incrementPassCount = function(){
+        $scope.passlimit++;
+    }
+    $scope.checkPass = function(password){
+        if(passStore.includes(password)){
+            $scope.password=null;
+            $scope.success = true;
+        }else{
+            $scope.incrementPassCount();
+            $scope.disablePass = $scope.passlimit > 4;
+        }
     }
 });
