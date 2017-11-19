7f4bafbea99be6b692da72878459706440c3794e
System Administrator <root@joshs-mbp.attlocal.net>
2017-11-19 13:22:56 -0500

    Comments added to xxs

--- scripts/xxsCtrl.js
+++ scripts/xxsCtrl.js
@@ -1,8 +1,10 @@
 var app = angular.module("myApp");
 
 app.controller('xxsCtrl', ['$scope', '$sce', function($scope, $sce){
+	//below is code to be sanatized
     $scope.code = '<span onmouseover="this.textContent=&quot;Got Ya! ' +
         '&quot;">Hover over this text.</span>';
+       //trust function never run for safe website
     $scope.trust = function() {
         return $sce.trustAsHtml($scope.code);
     }
